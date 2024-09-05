e9e8caff38b15596d56ca902b912fe794acfb2bf
Biswajit Padhan <50108200+biswajitpadhan@users.noreply.github.com>
2024-09-05 15:53:41 +0530

    Update welcome.groovy

--- vars/welcome.groovy
+++ vars/welcome.groovy
@@ -1,6 +1,6 @@
 def add(x,y){
-	echo "sum of two value ${x},and ${y} is ${x+y}" 
+	echo "sum of two value ${x},and ${y} is equal to ${x+y}" 
 }
 def mul(x,y){
-	echo "multiplication of two value ${x},and ${y} is ${x*y}" 
+	echo "multiplication of two value ${x},and ${y} is equal to ${x*y}" 
 }
